dc695c452a6807eca132e98afe9c2cae8340b56e
Chris Vincent <project802@users.noreply.github.com>
2019-01-09 12:08:20 -0500

    Utilize the ArduinoJson library and improve retry mechanism

--- WorldTimeAPI.h
+++ WorldTimeAPI.h
@@ -21,16 +21,18 @@ class WorldTimeAPI {
     };
     
   private:
-    const char *    _serverBaseURL      = "http://worldtimeapi.org/api";
-    unsigned long   _updateIntervalMs   = (24*60*60*1000);
-    bool            _receivedTime       = false;
-    
-    long            _utcOffsetS         = 0;
-    unsigned long   _timeSinceEpochS    = 0;
-    unsigned long   _lastUpdateMs       = 0;
+    const char *          _serverBaseURL      = "http://worldtimeapi.org/api";
+    const unsigned long   _updateIntervalMs   = (24*60*60*1000);
+    const unsigned long   _retryIntervalMs    = (60*1000);
 
-    TimeMethod      _timeMethod;
-    String          _timezone;
+    bool                  _receivedTime       = false;
+    long                  _utcOffsetS         = 0;
+    unsigned long         _timeSinceEpochS    = 0;
+    unsigned long         _lastUpdateMs       = 0;
+    unsigned long         _lastAttemptMs      = 0;
+
+    TimeMethod            _timeMethod;
+    String                _timezone;
     
   public:
     WorldTimeAPI( TimeMethod method = TIME_USING_IP, String timezone = "America/New_York" );
@@ -39,10 +41,6 @@ class WorldTimeAPI {
     bool timeReceived()
     {
       return this->_receivedTime;
-    }
-    void setUtcOffset( long offsetSeconds )
-    {
-      this->_utcOffsetS = offsetSeconds;
     }
     
     unsigned long getUnixTime();
