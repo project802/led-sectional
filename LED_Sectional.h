fbd0a65e6af18da1ddae5cb4867acfe98a2a5b16
project802 <chris@project802.net>
2019-01-14 20:54:56 -0500

    Set intensity for 50 lux at 8.

--- LED_Sectional.h
+++ LED_Sectional.h
@@ -41,7 +41,7 @@
     // Lux,   Intensity
     { 0,      0 },                    // REQUIRED index of 0
     { 1,      4 },                    // Fill in any points desired to define the curve.
-    { 50,     4 },                    // Spaces between points are linearly interpreted.
+    { 50,     8 },                    // Spaces between points are linearly interpreted.
     { 125,    16 },                   // Use SECTIONAL_DEBUG to print out sensor values to help tuning.
     { 300,    32 },                   // First and last index are required but the values can be changed.
     { 65536,  128 }                   // REQUIRED index of 65536
